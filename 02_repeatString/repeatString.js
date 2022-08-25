62b37f19d783358c1e06471f3f6eaa28058bb94b
katie <katiepb525@gmail.com>
2022-08-25 13:15:30 -0500

    pass first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string = 'hey', num = 3) {
+    newString = "";
+    for (let i = 0; i < num; i++) {
+        newString += string;
+    }
 
+    return newString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
