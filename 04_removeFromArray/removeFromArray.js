5b5bf4102be7365920ba8efaa856a88588b992a5
katie <katiepb525@gmail.com>
2022-08-25 14:14:45 -0500

    first try

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, toRemove) {
+    let indexRemoved = array.indexOf(toRemove);
+    array.splice(indexRemoved);
+    return array;
 };
 
 // Do not edit below this line
