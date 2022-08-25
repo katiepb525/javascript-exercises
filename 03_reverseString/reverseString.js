3a3233cd02dc52d03644b3c0ac5ed2912c134b30
katie <katiepb525@gmail.com>
2022-08-25 14:04:54 -0500

    pass second test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function (string) {
     let reversedString = ""; // initalize a blank string variable to store value into
 
-    for (i = string.length; i >= 0; i--) { // set i as length of string (so hello =  5) while 5 is equal to or less than 0; decrement i before the end of the loop.
+    for (i = string.length; i >= 0; i--) { // set i as length of string (so hello =  5) while 5 is equal to or less than 0; decrement i after each iteration.
         reversedString += string.charAt(i);
     }
     return reversedString;
