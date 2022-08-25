e49e22a7ff9678653972c18c96a2e77a73b05f7e
katie <katiepb525@gmail.com>
2022-08-25 13:38:21 -0500

    finish all tests in repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
 const repeatString = function (string = 'hey', num = 3) {
     newString = "";
+
+    if (num < 0) {
+        newString = "ERROR"
+        return newString;
+    }
+
     for (let i = 0; i < num; i++) {
         newString += string;
     }
