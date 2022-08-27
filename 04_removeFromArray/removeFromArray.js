70f2c1b25691b691c01ed113a59ea37f5e2e0150
katie <katiepb525@gmail.com>
2022-08-27 12:19:17 -0500

    create loop to cycle through items in array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
 const removeFromArray = function (array, ...args) {
 
-    for (i = 0; i < args.length; i++) {
+    // loop if items in array are present in args
+
+    for (i = 0; i < array.length; i++) {
+        // if item in array does equal item in args, continue with removal
+
+        if 
+        
+    }
+
+
+    // if item in array does not equal item in args, ignore and continue loop 
+
+
+    // only run if items in given array are 
+
+    for (i = 0; i < args.length; i++) { // loop through the entire length of current arguments
         const removeIndex = array.indexOf(args[i]); // find the index of the current item in array
         array.splice(removeIndex, 1); // remove the item once using the index
     }
