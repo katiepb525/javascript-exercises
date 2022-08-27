82cd582278c1b86f50e7a1ffe75ffb7d0025a3c0
katie <katiepb525@gmail.com>
2022-08-27 14:12:06 -0500

    make a copy of original array and store in its own variable

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,13 @@
 const removeFromArray = function (array, ...args) {
-    let origArrayLength = array.length;
+    let origArray = Array.from(array);
     // loop through each item in original array
-    for (let i = 0; i < origArrayLength; i++) {
+    for (let i = 0; i < origArray.length; i++) {
 
         let currentVal = i;
-        // loop argumenets through current value
+        // loop arguments through current value
         for (let j = 0; j < args.length; j++) {
 
-            if (array[currentVal] === args[j]) {
+            if (origArray[currentVal] === args[j]) {
                 let removeIndex = array.indexOf(args[j]); // find the index of the current item in array
                 array.splice(removeIndex, 1); // remove the item once using the index
             }
