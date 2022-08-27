1acb3c44f9e2e130b5cb513b01016bac666fc3da
katie <katiepb525@gmail.com>
2022-08-27 12:17:26 -0500

    pass 2nd test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,11 @@
-const removeFromArray = function (array, toRemove) {
-    const indexRemoved = array.indexOf(toRemove);
-    array.splice(indexRemoved);
-    return array;
+const removeFromArray = function (array, ...args) {
+
+    for (i = 0; i < args.length; i++) {
+        const removeIndex = array.indexOf(args[i]); // find the index of the current item in array
+        array.splice(removeIndex, 1); // remove the item once using the index
+    }
+
+    return array; // return the edited array
 };
 
 // Do not edit below this line
