5995320bdba0007db30473427a96286cdacf61b9
katie <katiepb525@gmail.com>
2022-08-27 13:48:15 -0500

    keep track of current value in array and loop items through value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,28 +1,18 @@
 const removeFromArray = function (array, ...args) {
+    let origArrayLength = array.length;
+    // loop through each item in original array
+    for (let i = 0; i < origArrayLength; i++) {
 
-    // loop if items in array are present in args
+        let currentVal = i;
+        // loop through each item in arguments
+        for (let j = 0; j < args.length; j++) {
 
-    for (i = 0; i < array.length; i++) {
-        // if item in array does equal item in args, continue with removal
-        if (array[i] == args[i]) {
-            const removeIndex = array.indexOf(args[i]); // find the index of the current item in array
-
-            array.splice(removeIndex, 1); // remove the item once using the index
+            if (currentVal === args[j]) {
+                let removeIndex = array.indexOf(args[j]); // find the index of the current item in array
+                array.splice(removeIndex, 1); // remove the item once using the index
+            }
         }
-
-
     }
-
-
-    // if item in array does not equal item in args, ignore and continue loop 
-
-
-    // only run if items in given array are 
-
-    // for (i = 0; i < args.length; i++) { // loop through the entire length of current arguments
-    // const removeIndex = array.indexOf(args[i]); // find the index of the current item in array
-    // array.splice(removeIndex, 1); // remove the item once using the index
-
 
     return array; // return the edited array
 };
