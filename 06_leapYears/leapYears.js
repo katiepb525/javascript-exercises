0dcd1ec1e7c586c20ba86d5e37c3b6c111b4f707
katie <katiepb525@gmail.com>
2022-08-28 11:21:31 -0500

    pass all tests!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,13 @@
 const leapYears = function (year) {
+    // check if year is divisible by four and not divisible by 100
     if (year % 4 === 0 && (!(year % 100 === 0))) {
         return true;
     }
+    // if year is divisible by 100, check if its divisible by 400
     else if ((year % 100 === 0) && (year % 400 === 0)) {
         return true;
     }
+    // in all other cases
     else {
         return false;
     }
