32c278cf4e73ea6236d90c416905d680664c910c
katie <katiepb525@gmail.com>
2022-08-28 10:03:20 -0500

    change '<' to '<='

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,14 +4,14 @@ const sumAll = function (num1, num2) {
     let result = 0;
 
     //loop through every number between num1 and num2
-    for (i = num1; i < num2; i++) {
+    for (i = num1; i <= num2; i++) {
         // add each number to the result
-
+        result += i;
 
     }
 
     // return the result after loop is finished
-
+    return result;
 };
 
 // Do not edit below this line
