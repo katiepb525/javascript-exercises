bf913cc5ca06dd173dfd90da9d4d5ced9cedf744
katie <katiepb525@gmail.com>
2022-08-28 11:19:25 -0500

    add comments

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,16 +1,12 @@
 const leapYears = function (year) {
-    if (year % 4 === 0) {
-        if (!(year % 100 === 0)) {
-            if (year % 400 === 0) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-
+    if (year % 4 === 0 && (!(year % 100 === 0))) {
         return true;
-
+    }
+    else if ((year % 100 === 0) && (year % 400 === 0)) {
+        return true;
+    }
+    else {
+        return false;
     }
 };
 
