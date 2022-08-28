3990449cf1d91cab60469dcea38165991ce6a790
katie <katiepb525@gmail.com>
2022-08-28 12:51:53 -0500

    first try

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (tempF) {
+  let tempC = 0;
+  tempC = (tempF - 32) * (5 / 9);
+  tempC = Math.round(tempC * 10) / 10;
+  return tempC;
 };
 
-const ctof = function() {
+const ctof = function (tempC) {
+  let tempF = 0;
+  tempF = tempC * (9 / 5) + 32;
+  tempF = Math.round(tempF * 10) / 10;
+  return tempF;
 
 };
 
