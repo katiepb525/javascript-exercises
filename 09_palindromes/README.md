3d74c22364a3e87343c5e765208823476ef3e459
katie <katiepb525@gmail.com>
2022-09-18 16:20:06 -0500

    removes punctuation

--- 09_palindromes/README.md
+++ 09_palindromes/README.md
@@ -2,7 +2,7 @@
 
 Write a function that determines whether or not a given string is a palindrome.
 
-A palindrome is a string that is spelled the same both forwards and backwards, usually without considering punctuation or word breaks:
+A palindrome is a string that is spelled the same both forwards and backwards, *usually without considering punctuation or word breaks:*
 
 ### some palindromes:
   - A car, a man, a maraca.
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,30 +1,31 @@
 const palindromes = function (rawString) {
-    // //create a regex of punctuation.spaces to look out for
-    // const punctuation = '/\s/!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    // //remove punctuation of string
-    // let splitString = rawString.replace(punctuation, "").toLowerCase();
-    // // rejoin split array
-    // splitString.join();
-    // // store modified original
-    // let origString = splitString;
-
-    let origString = rawString;
+    //create a regex of punctuation.spaces to look out for
+    const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    //remove punctuation of string
+    let cleanString = rawString.replace(punctuation, "").toLowerCase();
+    // store modified original
+    let origString = cleanString;
 
     // reverse the original string and store in variable
     let reversedString = ""; // initalize a blank string variable to store value into
 
     for (i = origString.length; i >= 0; i--) { // set i as length of string (so hello =  5) while 5 is equal to or less than 0; decrement i after each iteration.
-        reversedString += rawString.charAt(i);
+        reversedString += origString.charAt(i);
     }
+
 
     // compare the original string with the reversed one
     if (origString === reversedString) {
         // if theyre the same, return true, 
+        console.log(`true ${reversedString}`);
+        console.log(`true ${origString}`);
 
         return true;
     }
     else if (origString != reversedString) {
         // if theyre different, return false
+        console.log(`false ${reversedString}`);
+        console.log(`false ${origString}`);
 
         return false;
     }
