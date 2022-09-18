5c528175cc34db3cf0b5ade979a19f4face7bf4f
katie <katiepb525@gmail.com>
2022-09-18 15:49:35 -0500

    bang (non working first try)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,29 @@
-const palindromes = function () {
+const palindromes = function (rawString) {
+    //remove punctuation and spaces of string
+    let splitString = rawString.replace(' ', ',');
+    // rejoin split array
+    splitString.join();
+    // store modified original
+    let origString = splitString;
+    // reverse the original string and store in variable
+
+    let reversedString = ""; // initalize a blank string variable to store value into
+
+    for (i = origString.length; i >= 0; i--) { // set i as length of string (so hello =  5) while 5 is equal to or less than 0; decrement i after each iteration.
+        reversedString += rawString.charAt(i);
+    }
+
+    // compare the original string with the reversed one
+    if (origString === reversedString) {
+        // if theyre the same, return true, 
+
+        return true;
+    }
+    else if (origString != reversedString) {
+        // if theyre different, return false
+
+        return false;
+    }
 
 };
 
