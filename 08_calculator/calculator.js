722813be78dd8a85debc2cdf14cf3b02c042a516
katie <katiepb525@gmail.com>
2022-09-18 15:14:37 -0500

    pass factorial test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -31,8 +31,21 @@ const power = function (a, b) {
   return product;
 };
 
-const factorial = function () {
+const factorial = function (a) {
 
+  // there is always one way to arrange a bunch of items in a set, so result by default is one.
+  // you can arrange 0 items in a set one way because it is an empty set, so this justifies setting the default to one...
+  if (a == 0 || a == 1) {
+    return 1;
+  }
+  result = 1;
+
+  //loop through each number in a set of /a/
+  for (i = 1; i <= a; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
