8b1b4c3a2567b7781d9797897046f207c8c4a74c
katie <katiepb525@gmail.com>
2022-09-19 19:24:33 -0500

    passed test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (array) {
+    // grab the list, then grab each element from the list, then grab the property of that element
+    let finalArray = array.map((element) => element.title);
+    return finalArray;
+    // objects have elements, elements have attributes and properties
+}
 
-};
 
 // Do not edit below this line
 module.exports = getTheTitles;
