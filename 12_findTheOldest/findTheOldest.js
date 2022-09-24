d9aef9aebfb885c6d0a836b697a8d65348a90496
katie <katiepb525@gmail.com>
2022-09-24 17:43:50 -0500

    remove old code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,23 +2,7 @@ const findTheOldest = function (obj) {
     // get the current year
     const currentYear = (new Date()).getFullYear();
 
-    // look and compare two elements at a time in the array
-    // return obj.reduce((olderPerson, currentPerson) => {
-    //     // store current oldest age
-    //     let oldestAge = olderPerson.yearOfDeath - olderPerson.yearOfBirth;
-    //     // store current age
-    //     let currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
-    //     // compare the two
-    //     if (oldestAge < currentAge) {
-    //         // set the oldest person to the newer oldest person, because the current value has "outcompeted" the previous one
-    //         olderPerson = currentPerson;
-    //     }
-    //     // return the element in the array that has "outcompeted" all others
-    //     return olderPerson;
-    //     // return doesnt end the function here... just returns the last iteration of "olderPerson" or the total value
-    // });
-
-    // modified to work with a living person...
+    // looping through the given array, reduce to the oldest person
     return obj.reduce((olderPerson, currentPerson) => {
         // store current oldest age
         // check if current oldest age has a date of death..
