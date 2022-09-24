f7b5f8dfb6381005f77391f75b0912608de17a5b
katie <katiepb525@gmail.com>
2022-09-24 17:08:43 -0500

    refactored function with some help from discord, using and understanding reduce()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
 const findTheOldest = function (obj) {
-    //get the current year
-    const currentYear = (new Date()).getFullYear();
+    // get the current year
+    // const currentYear = (new Date()).getFullYear();
+
+
+    // look and compare two elements at a time in the array
+    return obj.reduce((olderPerson, currentPerson) => {
+        // store current oldest age
+        let oldestAge = olderPerson.yearOfDeath - olderPerson.yearOfBirth;
+        // store current age
+        let currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        // compare the two
+        if (oldestAge < currentAge) {
+            // set the oldest person to the newer oldest person, because the current value has "outcompeted" the previous one
+            olderPerson = currentPerson;
+        }
+        // return the element in the array that has "outcompeted" all others
+        return olderPerson;
+    });
 
 };
 
